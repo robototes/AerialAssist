1f75aa5ff171e59a75b47f7d4fd5dfe379ab3adc
Max Orth <max.orth96@gmail.com>
2014-01-06 21:41:30 -0800

    initial

--- AerialAssist/src/com/shsrobotics/AerialAssist/Main.java
+++ AerialAssist/src/com/shsrobotics/AerialAssist/Main.java
@@ -6,6 +6,7 @@ package com.shsrobotics.AerialAssist;
 
 import com.shsrobotics.library.FRCRobot;
 import edu.wpi.first.wpilibj.camera.AxisCameraException;
+import edu.wpi.first.wpilibj.image.NIVision;
 import edu.wpi.first.wpilibj.image.NIVisionException;
 
 /**
@@ -15,13 +16,31 @@ import edu.wpi.first.wpilibj.image.NIVisionException;
 public class Main extends FRCRobot implements Maps {
     
     
+    
     public void robotInit() {
-        
+        try {
+            Images.startImage = camera.getImage();
+        }
+        catch (AxisCameraException ace) {
+            
+        }
+        catch (NIVisionException nive) {
+            
+        }
         
     }
     
     public void autonomousInit() {
-        
+        try {
+            Images.afterImage = camera.getImage();
+            NIVision.
+        }
+        catch (AxisCameraException ace) {
+            
+        }
+        catch (NIVisionException nive) {
+            
+        }
     }
     
     public void autonomousPeriodic() {
--- AerialAssist/src/com/shsrobotics/AerialAssist/Maps.java
+++ AerialAssist/src/com/shsrobotics/AerialAssist/Maps.java
@@ -8,8 +8,8 @@ import edu.wpi.first.wpilibj.image.Image;
  * @author Max
  */
 public interface Maps extends Hardware {
-    public static final class Stuff {
-        public static Image startPicture;
+    public static final class Images {
+        public static Image startImage;
         public static Image afterImage;
     }
 }
