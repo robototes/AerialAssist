0c0e00c449c295e6c2b6529b21ad56b0fd147eec
Cory McCartan <cory.mccartan@gmail.com>
2014-01-07 16:04:13 -0800

    cleanup

--- AerialAssist/src/com/shsrobotics/AerialAssist/Maps.java
+++ AerialAssist/src/com/shsrobotics/AerialAssist/Maps.java
@@ -1,11 +1,9 @@
-
 package com.shsrobotics.AerialAssist;
 
 import edu.wpi.first.wpilibj.image.Image;
 
 /**
- *
- * @author Max
+ * @author Team 2412
  */
 public interface Maps extends Hardware {
     public static final class Images {
