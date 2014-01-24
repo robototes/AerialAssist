8580e708ccbc5c1b1cd36c571c60d2c0e2c20f71
robototes2412 <robototes2412@gmail.com>
2014-01-24 15:38:55 -0800

    IIR

--- AerialAssist/src/com/shsrobotics/AerialAssist/Drive.java
+++ AerialAssist/src/com/shsrobotics/AerialAssist/Drive.java
@@ -1,22 +1,31 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.shsrobotics.AerialAssist;
+
+import com.sun.squawk.util.MathUtils;
 
 /**
  *
  * @author RoboTotes Team 2412
  */
 public class Drive implements Hardware {
+	public static IIR xIIR = new IIR();
+	public static IIR yIIR = new IIR();
 	public static void drive() {
+		double x = xIIR.output(driveStick.getX());
+		double y = yIIR.output(driveStick.getY());
+		x = MathUtils.pow(x, 3);
+		y = MathUtils.pow(y, 5);
+		
 		if (!Maps.Buttons.driveScale.held()) {
-			DriveBase.drive.arcadeDrive(driveScaleFunction(driveStick.getX()), driveScaleFunction(noGyroscope));
+			DriveBase.drive.arcadeDrive(driveScaleFunction(x), driveScaleFunction(y));
 		}
 		else {
-			DriveBase.drive.arcadeDrive(Hardware.driveStick.getX(), driveStick.getY());
+			DriveBase.drive.arcadeDrive(x, y);
 		}
+		
 	}
+	
+	// x value iir cubing
+	// y value iir quinting
 	
 	private static double driveScaleFunction(double x) {
 		return (x-Maps.Drive.DRIVE_SCALE)*(x-Maps.Drive.DRIVE_SCALE)*(x-Maps.Drive.DRIVE_SCALE) -  Maps.Drive.DRIVE_SCALE;
